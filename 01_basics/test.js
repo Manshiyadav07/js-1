7f1982a12e8e1480d911f8bb99ff4be09d988f0b
Manshiyadav07 <manshi111etw@gmail.com>
2025-06-07 07:19:10 +0000

    datatypes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,17 @@
-console.log("manshi");
+"use strict"; // treat all js code as newer version 
+
+//alert(3+3) // we are using node js ,not browser
+
+console.log(3+3);
+console.log("ml");
+
+//datatypes 
+//number=> 2 to power 53
+//bigint 
+// null -> stand alone value 
+// undefined 
+// symbols => unique 
+
+//objects 
+console.log(typeof(null)); // object hain iska type apne aap mein
+
