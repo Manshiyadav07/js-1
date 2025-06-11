e7b62b67d72c5e4be51e6610e99a2df7aeffa73d
Manshiyadav07 <manshi111etw@gmail.com>
2025-06-11 08:19:53 +0000

    modified conversions and dates

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -1,17 +1,90 @@
-// let score ="33"
-// console.log(typeof score);
-// console.log(typeof (score));
+// // let score ="33"
+// // console.log(typeof score);
+// // console.log(typeof (score));
 
-// let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
 
-/*
-"33" =>33
-"33abc"=> NaN  not a number 
-true =1; false = 0 
-*/
-let isLoggedIn =1
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn )
 
-//1-> true , 0-> false ,manshi->true 
+// // let valueInNumber = Number(score)
+// // console.log(typeof valueInNumber);
+
+// /*
+// "33" =>33
+// "33abc"=> NaN  not a number 
+// true =1; false = 0 
+// */
+// let isLoggedIn =1
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// // console.log(booleanIsLoggedIn )
+
+// //1-> true , 0-> false ,manshi->true 
+// let value =3
+// let negVlaue = -value
+// //console.log(negVlaue);
+
+// // console.log("1"+2);
+// // console.log(1+"2");
+// // console.log("1"+2+2);
+// // console.log(1+2+"2");
+// // console.log(+true);
+// // console.log(+" ");
+
+
+// // let x=10;
+// // console.log(x++);
+
+
+// // let y=10;
+// // console.log(++y);
+
+// //Stack(primitive) ,Heap (Non-primitive)
+// let manshi ="manshi11etw"
+// let myyt =manshi 
+// myyt = "lakshi"
+// // console.log(manshi);
+// // console.log(myyt);
+
+// let user={
+//     email : "manshi11etw",
+//     upi : "userbl",
+// }
+// let userTwo = user 
+// userTwo.email = " manshi@microsoft"
+// console.log(user.email);
+// console.log(userTwo.email);
+
+
+// const name =" manshi "
+// const repo =50
+// console.log(name + repo );
+// console.log(`my name is ${name} and my ${repo}`)
+
+// const gameName = new String ("His");
+// console.log(gameName.toUpperCase())
+// console.log(charAt(2));
+
+// const newStringOne ="    Hitesh   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url =" https"
+
+// const score =400;
+// const balance = new Number(100);
+// console.log(balance.toString());
+
+// const nam = 23.444
+// console.log(nam.toPrecision(3));
+
+//Dates 
+
+let mydate =new Date();
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+
+
+//let myCreatedDate = new Date("01-15-2025");
+//console.log(myCreatedDate.toLocaleString())
+let newdate = new Date();
+console.log(newdate.getFullYear());
+
+
